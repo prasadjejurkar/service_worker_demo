bb6a17edef791ef09a71cf81bee2b20acd6ac2bc
prasadjejurkar <prasad.jejurkar@triarqhealth.com>
2020-03-02 10:34:15 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { SwPush } from '@angular/service-worker';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sw';
+
+  data: any[] = [];
+
+  constructor(private http: HttpClient, readonly swPush: SwPush) {
+    // this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts')
+    //   .subscribe((response) => {
+    //     this.data = response;
+    //   })
+    // this.swPush.requestSubscription({ serverPublicKey: 'BEc27NCrwg2eiR5Xv7Z3iJAsPdmTODl0PshC5zsPeyP9pxy3yxBzCOxIfFP9I7EAipUOL10m8ihIRjPJwfR4Ksc' })
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch(err => console.error("Could not subscribe to notifications", err));;
+
+    // this.subscribeToPush();
+    // this.swPush.notificationClicks.subscribe(
+    //   ({ action, notification }) => {
+    //     console.log("notification click");
+
+    //     // TODO: Do something in response to notification click.
+    //   });
+  }
+
+  // private async subscribeToPush() {
+  //   try {
+  //     const sub = await this.swPush.requestSubscription({
+  //       serverPublicKey: 'BEc27NCrwg2eiR5Xv7Z3iJAsPdmTODl0PshC5zsPeyP9pxy3yxBzCOxIfFP9I7EAipUOL10m8ihIRjPJwfR4Ksc',
+  //     });
+  //     // TODO: Send to server.
+  //   } catch (err) {
+  //     console.error('Could not subscribe due to:', err);
+  //   }
+  // }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule, SwUpdate, SwPush } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+import { MatSnackBarModule, MatSnackBar } from '@angular/material'
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 
 @NgModule({
   declarations: [
@@ -10,9 +18,42 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private update: SwUpdate, private push: SwPush, private snacbar: MatSnackBar) {
+    console.log("AppModule");
+
+    update.available.subscribe((update) => {
+      console.log('udpate available');
+      const snack = snacbar.open('Update Available', 'Reload');
+      snack
+        .onAction()
+        .subscribe(() => {
+          window.location.reload();
+        })
+      push.messages.subscribe((msg) => {
+        console.log(msg);
+        snacbar.open(JSON.stringify(msg));
+      })
+
+      const key = 'BEc27NCrwg2eiR5Xv7Z3iJAsPdmTODl0PshC5zsPeyP9pxy3yxBzCOxIfFP9I7EAipUOL10m8ihIRjPJwfR4Ksc';
+      push.requestSubscription({ serverPublicKey: key })
+        .then(pushSubscription => {
+          console.log("pushSubscription");
+          console.log(pushSubscription.toJSON());
+
+        })
+    })
+  }
+}
+
+
